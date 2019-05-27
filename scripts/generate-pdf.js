b654795639c79cff18c5bfd32bd0dec04878cc75
umxr <umarg1997@gmail.com>
2019-05-27 20:24:10 +0100

    basic version done

---
+++ scripts/generate-pdf.js
@@ -0,0 +1,17 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const height = await page.evaluate(
+    () => document.documentElement.offsetHeight,
+  );
+  await page.goto('http://localhost:3000/');
+  await page.emulateMedia('screen');
+  await page.pdf({
+    path: './react.pdf', // path (relative to CWD) to save the PDF to.
+    printBackground: true, // print background colors
+    height: height,
+  });
+  await browser.close();
+})();

89cf2008f04834aa79922173aaf9f28ab0c4951b
umxr <umarg1997@gmail.com>
2019-06-09 20:50:07 +0100

    done........

--- scripts/generate-pdf.js
+++ scripts/generate-pdf.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer');
 
+const route = process.argv[2];
+
 (async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const height = await page.evaluate(
     () => document.documentElement.offsetHeight
   );
-  await page.goto('http://localhost:3000/pdf/R204');
+  await page.goto(`http://localhost:3000/pdf/${route}`);
   await page.emulateMedia('screen');
   await page.pdf({
     path: './react.pdf', // path (relative to CWD) to save the PDF to.
